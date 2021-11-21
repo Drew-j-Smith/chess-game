1a804b6375f33503671c791f150be9d9ea9f4d98
Drew Smith <smithdrew555@yahoo.com>
2021-11-21 11:24:40 -0500

    added piece movement and json loading

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function generateBoard() {
+$(function generateBoard() {
     $(".board").css("grid-template-columns", "100px ".repeat(8));
     $(".board").css("grid-template-rows", "100px ".repeat(8));
 
@@ -8,10 +8,44 @@ function generateBoard() {
         }
     }
     $(".board").children("*").addClass("square");
+});
+
+function pieceMovement() {
+    $(".piece").draggable({
+        scroll: false,
+        revert: "invalid",
+        revertDuration: 0
+    });
+    $(".piece").mousedown(function () {
+        $(this).css('z-index', '1000');
+    });
+    $(".piece").mouseup(function () {
+        $(this).css('z-index', '1');
+    });
+    $(".piece").droppable({
+        accept: function (dropElem) {
+            return true;
+        },
+        drop: function (event, ui) {
+            if ($(this) !== $(ui.draggable)) {
+                var parent = $(this).parent();
+                $(this).remove();
+                parent.append(ui.draggable);
+                $(ui.draggable).css({ "left": 0, "top": 0 });
+            }
+        }
+    });
+    $(".square").droppable({
+        accept: function (dropElem) {
+            return true;
+        },
+        drop: function (event, ui) {
+            // $(this).children().remove();
+            $(this).append(ui.draggable);
+            $(ui.draggable).css({ "left": 0, "top": 0 });
+        }
+    });
 }
-
-$(function () { generateBoard(); });
-
 
 function clearBoard() {
     $(".board").children("*").children("*").remove();
@@ -21,84 +55,24 @@ $(function generatePieces() {
     loadFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 })
 
-function loadFen(fen) {
+async function loadFen(fen) {
     clearBoard();
     var pos = 0;
+    let data = await $.getJSON("./pictures.json");
     for (var i = 0; i < fen.length; i++) {
         if (pos >= 64)
             break;
-        switch (fen[i]) {
-            case "r":
-                $("." + pos).append("<img src=\"./cburnett/bR.svg\">");
-                pos++;
-                break;
-            case "n":
-                $("." + pos).append("<img src=\"./cburnett/bN.svg\">");
-                pos++;
-                break;
-            case "b":
-                $("." + pos).append("<img src=\"./cburnett/bB.svg\">");
-                pos++;
-                break;
-            case "q":
-                $("." + pos).append("<img src=\"./cburnett/bQ.svg\">");
-                pos++;
-                break;
-            case "k":
-                $("." + pos).append("<img src=\"./cburnett/bK.svg\">");
-                pos++;
-                break;
-            case "p":
-                $("." + pos).append("<img src=\"./cburnett/bP.svg\">");
-                pos++;
-                break;
-
-            case "R":
-                $("." + pos).append("<img src=\"./cburnett/wR.svg\">");
-                pos++;
-                break;
-            case "N":
-                $("." + pos).append("<img src=\"./cburnett/wN.svg\">");
-                pos++;
-                break;
-            case "B":
-                $("." + pos).append("<img src=\"./cburnett/wB.svg\">");
-                pos++;
-                break;
-            case "Q":
-                $("." + pos).append("<img src=\"./cburnett/wQ.svg\">");
-                pos++;
-                break;
-            case "K":
-                $("." + pos).append("<img src=\"./cburnett/wK.svg\">");
-                pos++;
-                break;
-            case "P":
-                $("." + pos).append("<img src=\"./cburnett/wP.svg\">");
-                pos++;
-                break;
-
-            case "/":
-                break;
-
-            default:
-                pos += parseInt(fen[i], 10);
+        if (fen[i] in data) {
+            $("." + pos).append("<img src=\"" + data[fen[i]] + "\">");
+            pos++;
+            continue;
         }
+        if (fen[i] == "/") {
+            continue;
+        }
+        pos += parseInt(fen[i], 10);
     }
-    $(".board").children("*").children("*").addClass("piece");
+    $(".board").children().children().addClass("piece");
+    pieceMovement();
 }
-
-$(function pieceMovement() {
-    $(".piece").draggable({ scroll: false });
-    $(".piece").mousedown(function () {
-        $('.board').css('z-index', '1');
-        $(this).css('z-index', '1000');
-        $(this).attr("curr-pos", "position: relative; left: " + $(this).position().left + "; top: " + $(this).position().top + ";")
-    });
-    $(".piece").mouseup(function () {
-        $(this).attr("style", $(this).attr("curr-pos"))
-    });
-});
-
-
 
